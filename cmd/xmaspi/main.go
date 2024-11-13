adabdd7c2242242ae2413d620e0a79059269acd7
Marcel Behrmann <info@eldoran.dev>
2024-11-13 21:55:14 +0100

    implement basic command structure

--- cmd/xmaspi/main.go
+++ cmd/xmaspi/main.go
@@ -1,7 +1,40 @@
 package main
 
-import "fmt"
+import (
+	"context"
+	"fmt"
+	"github.com/eclipse/paho.golang/autopaho"
+	"os"
+	"os/signal"
+	"syscall"
+)
 
 func main() {
 	fmt.Println("starting xmaspi v3 - mqtt")
+
+	commandChannel := make(chan int)
+	defer close(commandChannel)
+
+	statusChannel := make(chan int)
+	defer close(statusChannel)
+
+	ctx, stop := signal.NotifyContext(context.Background(), os.Interrupt, syscall.SIGTERM)
+	defer stop()
+
+	pahoConfig := autopaho.ClientConfig{}
+
+	c, err := autopaho.NewConnection(ctx, pahoConfig)
+	if err != nil {
+		panic(err)
+	}
+
+	if err = c.AwaitConnection(ctx); err != nil {
+		panic(err)
+	}
+
+	// Wait for interrupt
+	<-ctx.Done()
+
+	// Await clean shutdown of mqtt client
+	<-c.Done()
 }
--- go.mod
+++ go.mod
@@ -1,3 +1,10 @@
 module github.com/EldoranDev/xmaspi/v3
 
 go 1.23.3
+
+require github.com/eclipse/paho.golang v0.22.0
+
+require (
+	github.com/gorilla/websocket v1.5.3 // indirect
+	golang.org/x/net v0.27.0 // indirect
+)
